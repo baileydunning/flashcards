df7cfb044f13cfe8dd954616cc7c16ffc14a522d
Bailey Dunning <bwdunning@gmail.com>
2020-10-07 19:00:58 -0600

    Initiate game test

---
+++ test/Game-test.js
@@ -0,0 +1,12 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/card');
+const Turn = require('../src/turn');
+const Deck = require('../src/deck');
+const Round = require('../src/round');
+const Game = require('../src/round');
+
+describe('Game', function() {
+
+});
